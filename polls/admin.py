b8c29277970e24f4537c4285a5f9ae07cf6ca3fe
Aditya <code.aditya@gmail.com>
2014-06-30 01:29:04 +0530

    change the order of fields in admin for Poll model

--- polls/admin.py
+++ polls/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from polls.models import Poll
 
-admin.site.register(Poll)
+
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+
+admin.site.register(Poll, PollAdmin)
