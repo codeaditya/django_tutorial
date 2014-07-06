422327a236546ffe30bcc71883b6daa054e1acc4
Aditya <code.aditya@gmail.com>
2014-07-06 17:04:17 +0530

    show the latest five polls on the index page

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from polls.models import Poll
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+    output = '<br>\n'.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
 
 
 def detail(request, poll_id):
