bec7053fbcaa78e6c5cfeeb0c62802736f1a5659
Aditya <code.aditya@gmail.com>
2014-07-06 17:25:29 +0530

    use template to render our index page

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 from django.shortcuts import render
 
 from polls.models import Poll
@@ -6,8 +7,11 @@ from polls.models import Poll
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = '<br>\n'.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, dict(
+        latest_poll_list=latest_poll_list,
+    ))
+    return HttpResponse(template.render(context))
 
 
 def detail(request, poll_id):
