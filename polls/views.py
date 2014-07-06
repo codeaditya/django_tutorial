89027b9a182d7d8ac2e3d225b78b53b8593f39b0
Aditya <code.aditya@gmail.com>
2014-07-06 16:37:38 +0530

    create more views for our Polls app dealing with details, results and voting

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
+
+
+def detail(request, poll_id):
+    return HttpResponse("You are looking at Poll {id}".format(id=poll_id))
+
+
+def results(request, poll_id):
+    return HttpResponse("You are looking at the results of Poll {id}"
+                        "".format(id=poll_id))
+
+
+def vote(request, poll_id):
+    return HttpResponse("You are voting on Poll {id}".format(id=poll_id))
