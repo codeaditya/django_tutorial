82ee91bd683ff7499cb36bce344d3070da58443e
Aditya <code.aditya@gmail.com>
2014-07-06 21:36:22 +0530

    create tests for Polls.was_published_recently() in case of old and recently published polls

--- polls/tests.py
+++ polls/tests.py
@@ -15,3 +15,19 @@ class PollMethodTest(TestCase):
         """
         future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
         self.assertFalse(future_poll.was_published_recently())
+
+    def test_was_published_recently_with_old_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is older than 1 day.
+        """
+        old_poll = Poll(pub_date=timezone.now() - datetime.timedelta(days=30))
+        self.assertFalse(old_poll.was_published_recently())
+
+    def test_was_published_recently_with_recent_poll(self):
+        """
+        was_published_recently() should return True for polls whose
+        pub_date is within the last day.
+        """
+        recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
+        self.assertTrue(recent_poll.was_published_recently())
