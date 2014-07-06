af28d638d532a7c02d3b0bc451dc9f457c24001c
Aditya <code.aditya@gmail.com>
2014-07-06 19:45:59 +0530

    modify vote view to record the vote and save it in database

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 
-from polls.models import Poll
+from polls.models import Choice, Poll
 
 
 def index(request):
@@ -22,4 +23,17 @@ def results(request, poll_id):
 
 
 def vote(request, poll_id):
-    return HttpResponse("You are voting on Poll {id}".format(id=poll_id))
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        context = dict(poll=poll, error_message="You didn't select a choice.")
+        # Redisplay the poll voting form
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
