6799ba485167e051b1ced2149a81d7d7c6906099
Aditya <code.aditya@gmail.com>
2014-07-07 00:04:09 +0530

    create tests for polls.views.DetailView for different scenarios

--- polls/tests.py
+++ polls/tests.py
@@ -78,6 +78,27 @@ class PollViewTest(TestCase):
         )
 
 
+class PollDetailViewTests(TestCase):
+
+    def test_detail_view_with_a_future_poll(self):
+        """
+        The detail view of a poll with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_poll = create_poll(question="Future poll.", days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_poll.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_poll(self):
+        """
+        The detail view of a poll with a pub_date in the past should
+        display the poll's question.
+        """
+        past_poll = create_poll(question="Past poll.", days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_poll.id,)))
+        self.assertContains(response, past_poll.question, status_code=200)
+
+
 class PollMethodTest(TestCase):
 
     def test_was_published_recently_with_future_poll(self):
